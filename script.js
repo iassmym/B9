dbacefcb93779371e468ff28ae9c2c03dd014a6c
Iasmym <iasmympatricia7@gmail.com>
2025-02-03 20:40:25 -0300

    atividade do dia

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-console.log("ola mundo")
 
-// declaração funcao
-//             parametros: o que estou recebendo
-function somar(num1, num2){
-    return num1 + num2;
-}
 
-// chamando a função
-//    argumentos: o que estou passando
-// somar(3,40);
-// somar(32,40);
-// somar(30,40);
-// somar(89,40);
-// somar(8963267637,40);
+ function exibir(nome){
+ document.write(` ${nome} ${nome.length}`)
+ }
+ var resultado = prompt("digite seu nome")
+exibir(resultado)
 
-// var numero = String(23)
-// var nome = "Beatriz" 
-// console.log(nome.toLowerCase());
-var teste = 90
-// document = html
-// document.write = escrevendo no HTML
-//document.write(`<h1 style="color: yellow">${somar(23,5)}</h1>`);// saida 
-//prompt // entrada
-
-// INTERPOLAÇÃO -> UNIR VARIAVEIS COM STRING
-// adicionar crase `` dentro das crases quando for utilizar
-// variaveis ou funções ${variavel/funcao}
-// ex: `<h1>${nomeUsuario}</h1>
-var nome = "Beatriz";
-var nomeJogo = "CS"
-console.log("ola " + nome + " seja bem vindo " + nomeJogo) // concatenando
-console.log(`ola ${nome} seja bem vindo ${nomeJogo}`) // interpolação
