e83bf87c3aa24e6b4e893f060a4452367b612516
Iasmym <iasmympatricia7@gmail.com>
2025-02-03 20:07:46 -0300

    exemplos de interpolaçao

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-console.log(ola, mundo)
+// console.log(ola, mundo)
 
-//declarando funçao
-function somar(numero1, numero2){
-    parseInt(numero1)
-    console.log(numero1, numero2)
+// //declarando funçao
+// function somar(numero1, numero2){
+    
+//      return numero1, numero2
+//     }
 
-}
+// //chmando funçao
 
-//chmando funçao
+// //somar(3,40);
 
-somar(3,40)
 
+// // var numero = String(23)
+// // var nome = "iasmym" / "IASMYM" | "Iasmym"
+// // console.log(nome.toLowerCase());
+// var teste = 90
+// document.write(`<h1 style="color: yellow">${somar(23,5)}</h1>`);
+// prompt 
+
+
+var nome= "iasmym";
+var nomeJogo = "cs"
+console.log("ola"+ nome + "seja bem vindo"+ nomeJogo)
+console.log(`ola ${nome} seja bem vindo ${nomeJogo}`)
