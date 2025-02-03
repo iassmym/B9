065a518a54880f2f6184a0b7c96e8da71b906e4d
Iasmym <iasmympatricia7@gmail.com>
2025-02-03 19:37:27 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log(ola, mundo)
+
+//declarando funçao
+function somar(numero1, numero2){
+    parseInt(numero1)
+    console.log(numero1, numero2)
+
+}
+
+//chmando funçao
+
+somar(3,40)
+
