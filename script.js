7b5c3218a2d70ec11b530906297ba2e8076397cc
Iasmym <iasmympatricia7@gmail.com>
2025-02-03 20:10:49 -0300

    copia bia

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-// console.log(ola, mundo)
+console.log("ola mundo")
 
-// //declarando funçao
-// function somar(numero1, numero2){
-    
-//      return numero1, numero2
-//     }
+// declaração funcao
+//             parametros: o que estou recebendo
+function somar(num1, num2){
+    return num1 + num2;
+}
 
-// //chmando funçao
+// chamando a função
+//    argumentos: o que estou passando
+// somar(3,40);
+// somar(32,40);
+// somar(30,40);
+// somar(89,40);
+// somar(8963267637,40);
 
-// //somar(3,40);
+// var numero = String(23)
+// var nome = "Beatriz" 
+// console.log(nome.toLowerCase());
+var teste = 90
+// document = html
+// document.write = escrevendo no HTML
+//document.write(`<h1 style="color: yellow">${somar(23,5)}</h1>`);// saida 
+//prompt // entrada
 
-
-// // var numero = String(23)
-// // var nome = "iasmym" / "IASMYM" | "Iasmym"
-// // console.log(nome.toLowerCase());
-// var teste = 90
-// document.write(`<h1 style="color: yellow">${somar(23,5)}</h1>`);
-// prompt 
-
-
-var nome= "iasmym";
-var nomeJogo = "cs"
-console.log("ola"+ nome + "seja bem vindo"+ nomeJogo)
-console.log(`ola ${nome} seja bem vindo ${nomeJogo}`)
+// INTERPOLAÇÃO -> UNIR VARIAVEIS COM STRING
+// adicionar crase `` dentro das crases quando for utilizar
+// variaveis ou funções ${variavel/funcao}
+// ex: `<h1>${nomeUsuario}</h1>
+var nome = "Beatriz";
+var nomeJogo = "CS"
+console.log("ola " + nome + " seja bem vindo " + nomeJogo) // concatenando
+console.log(`ola ${nome} seja bem vindo ${nomeJogo}`) // interpolação
